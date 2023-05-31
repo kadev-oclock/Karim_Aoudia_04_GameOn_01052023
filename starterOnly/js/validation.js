7b6089b220f881866a69fc20a94d3b84d9f857b9
kadev <karimaoudiaa@gmail.com>
2023-05-31 17:16:11 +0200

    fix:date

--- starterOnly/js/validation.js
+++ starterOnly/js/validation.js
@@ -19,7 +19,7 @@ formSubmit.addEventListener("submit", validate);
 
 // Fonction de validation et On empêche l'envoi du formulaire
 function validate(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
   const firstName = firstNameInput.value.trim();
   const lastName = lastNameInput.value.trim();
@@ -30,39 +30,55 @@ function validate(event) {
   const checkbox1Checked = checkbox1Input.checked;
 
   // Validation du champ Prénom
+  let firstNameValid = true;
+
   if (firstName.length < 2) {
+    firstNameValid = false;
+  } else if (!firstName.match(/^[a-zA-Z]{2,}$/)) {
+    firstNameValid = false;
+  }
+
+  if (firstNameValid == false) {
     errorMessage(
       firstNameError,
-      "Veuillez entrer 2 caractères ou plus pour le champ du prénom ."
+      "le prénom doit comporter au moins 2 lettres et pas de caratère spéciaux.",
+      "first"
     );
-    return false;
-  } else if (!firstName.match(/^[a-zA-Z]{2,}$/)) {
-    errorMessage(firstNameError, "prénom doit commencer par une lettre.");
-    return false;
+  } else {
+    errorMessage(firstNameError, "");
   }
 
   // Validation du champ Nom
+  let lastNameValid = true;
+
   if (lastName.length < 2) {
+    lastNameValid = false;
+  } else if (!lastName.match(/^[a-zA-Z]{2,}$/)) {
+    lastNameValid = false;
+  }
+
+  if (lastNameValid == false) {
     errorMessage(
       nameError,
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+      "le nom doit comporter au moins 2 lettres et pas de caractère spéciaux.",
+      "last"
     );
-    return false;
-  } else if (!lastName.match(/^[a-zA-Z]{2,}$/)) {
-    errorMessage(nameError, "nom doit commencer par une lettre.");
-    return false;
+  } else {
+    errorMessage(nameError, "");
   }
 
   // Validation de l'adresse e-mail
   if (!isValidEmail(email)) {
     errorMessage(emailError, "Veuillez entrer une adresse e-mail valide.");
-    return false;
+  } else {
+    errorMessage(emailError, "");
   }
 
   // Validation de la date de naissance
   if (!isValidDate(birthdate)) {
     errorMessage(birthdateError, "Vous devez entrer votre date de naissance.");
-    return false;
+  } else {
+    errorMessage(birthdateError, "");
   }
 
   // Validation du nombre de concours
@@ -71,7 +87,8 @@ function validate(event) {
       quantityError,
       "Veuillez entrer un nombre valide pour le nombre de concours (entre 0 et 99)."
     );
-    return false;
+  } else {
+    errorMessage(quantityError, "");
   }
 
   // Validation du choix de l'emplacement
@@ -84,48 +101,46 @@ function validate(event) {
 
   if (!locationChecked) {
     errorMessage(locationError, "Vous devez choisir une option.");
-    return false;
+  } else {
+    errorMessage(locationError, "");
   }
 
   // Validation de la case à cocher des conditions générales
   if (!checkbox1Checked) {
     errorMessage(
+      checkboxError,
       "Vous devez vérifier que vous acceptez les termes et conditions."
     );
-    return false;
+  } else {
+    errorMessage(checkboxError, "");
   }
 
-  
   // Code pour fermer le formulaire ici
   // document.querySelector(".btn-submit").value = "Fermer";
-  
+
   // Changer la valeur du bouton une fois le formulaire validé
   document.querySelector(".btn-submit").addEventListener("click", overlay());
-  
+
   // Après avoir effectué toutes les validations avec succès
   confirmationMessage.textContent = "Merci ! Votre réservation a été reçue.";
 
- return true;
+  return true;
 }
 
-
-
-
 //function message erreur
-function errorMessage(champ, message, isValid) {
-  if (!isValid) {
-    champ.textContent = message;
-  } else {
-    champ.reset();
-  }
-  validInput.forEach(function (input) {
-    if (!isValid) {
-      input.classList.toggle("errors");
+function errorMessage(champ, message, champInput) {
+  champ.textContent = message;
+
+  if (champInput) {
+    let toto = document.querySelector("#" + champInput);
+    console.log(toto);
+
+    if (message.length > 0) {
+      toto.classList.add("errors");
     } else {
-      input.style.display = "none";
+      toto.classList.remove("errors");
     }
-  });
-  
+  }
 }
 
 // Fonction de validation de l'adresse e-mail
@@ -146,17 +161,16 @@ function isValidQuantity(quantity) {
   return quantityPattern.test(quantity);
 }
 
-
 //fonction overlay quand le form est soumis
 function overlay() {
-const getClassName = modalBody.className;
-if (getClassName === "modal-body") {
-  modalBody.classList.add("hidden");
-  validModal.classList.remove("hidden");
-  formSubmit.reset();
+  // Fermer le formulaire
+ 
+  document.querySelector(".modal-body").classList.add("hidden-form");
+  document.querySelector(".modal-body").classList.remove("hidden-form");
+  document.querySelector(".modal-body").classList.remove("succes-form");
+  document.querySelector(".modal-body").classList.add("succes-form");
 
-} else {
-  modalBody.classList.remove("hidden");
-  validModal.classList.add("hidden");
 }
-}
+
+
+
